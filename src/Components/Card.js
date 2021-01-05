c40c4b2b7bb8a895b41e4502c3b80bef94dd36f0
karoltomaszewski <karoltomaszewski@users.noreply.github.com>
2021-01-05 19:28:12 +0100

    id

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,10 @@
 const Card = props =>{
     return(
         <div className="Card">
-           <h3>{props.city}</h3>
-          <p>{props.country}</p>
-          <p>{props.population}</p>
+            <p><strong>#{props.cardId}</strong></p>
+            <h3>{props.city}</h3>
+            <p>{props.country}</p>
+            <p>{props.population}</p>
         </div>
     );
 }
